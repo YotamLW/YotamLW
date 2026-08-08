8863feb28acb1f0c60c440df9fd5a93a9ea39f7e
YotamLW <yotamlw@gmail.com>
2026-08-09 00:32:18 +0100

    Strawberry stuff

---
+++ website/projects/pages/files.js
@@ -0,0 +1,12 @@
+// create html page 
+// add path (relative to pages) in one of the arrays below 
+// add link to parent page 
+
+let textFiles = [
+  "home",
+  "strawberry"
+]
+
+let codeFiles = [
+
+]
